11e6d3b6e5f2c5ada3ad5d70b1e69f65aa5ba6fc
gsudheesh <68291089+gsudheesh@users.noreply.github.com>
2025-12-19 15:48:48 +1100

    Update file paths for Excel and output folder

--- scraping/espn_scraper_loop.py
+++ scraping/espn_scraper_loop.py
@@ -3,10 +3,10 @@ import asyncio
 import time
 
 #  Path to your Excel file
-excel_path = "C:/Users/sudhe/OneDrive/Desktop/Social Web Analytics Group Project/match id.xlsx"
+excel_path = "../data/raw/match id.xlsx"
 
 #  Folder to save JSON outputs
-output_folder = "C:/Users/sudhe/OneDrive/Desktop/Social Web Analytics Group Project/rem_matches"
+output_folder = "../data/raw/rem_matches"
 
 #  Read the Excel file (sheet name optional if it's the first one)
 df = pd.read_excel(excel_path)
@@ -21,3 +21,4 @@ from espn_scraper import main  # if it's in the same file, you don't need to re-
 start = time.time()
 asyncio.run(main(output_folder, final_matches))
 print("Done! Time taken:", time.time() - start)
+
